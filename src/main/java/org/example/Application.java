6a5de43e414dd12bffe605427ecbed5e1ec20d5a
Art <you@example.com>
2020-11-30 05:51:49 +0200

    Code formating at Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -5,15 +5,10 @@ import quickfix.*;
 public interface Application
 {
     void onCreate( SessionID sessionID );
-
     void onLogon( SessionID sessionID );
-
     void onLogout( SessionID sessionID );
-
     void toAdmin( Message message, SessionID sessionID );
     void toApp( Message message, SessionID sessionID ) throws DoNotSend;
     void fromAdmin( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon;
     void fromApp( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon;
-
-
 }
