93f3cf22c715389aad14b3a2f0d73e616e35b3f4
Art <you@example.com>
2020-11-30 06:05:58 +0200

    Update FooApplication.java

--- src/main/java/org/example/FooApplication.java
+++ src/main/java/org/example/FooApplication.java
@@ -2,40 +2,45 @@ package org.example;
 
 import quickfix.*;
 
-public class FooApplication implements Application
+public class FooApplication implements  quickfix.Application
 {
+    void log( Object o )
+    {
+        System.out.println( o );
+    }
+
     public void onCreate( SessionID sessionID )
     {
-
+        log( "onCreate:" + sessionID );
     }
 
     public void onLogon( SessionID sessionID )
     {
-
+        log( "onLogon: " + sessionID );
     }
 
     public void onLogout( SessionID sessionID )
     {
-
+        log( "onLogout: " + sessionID );
     }
 
     public void toAdmin( Message message, SessionID sessionID )
     {
-
-    }
-
-    public void toApp( Message message, SessionID sessionID ) throws DoNotSend
-    {
-
+        log( "toAdmin: " + message );
     }
 
     public void fromAdmin( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon
     {
-
+        log( "fromAdmin: " + message );
     }
 
-    public void fromApp( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon
+    public void toApp( Message message, SessionID sessionID ) throws DoNotSend
     {
+        log( "toApp: " + message );
+    }
 
+    public void fromApp( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType
+    {
+        log( "fromApp: " + message );
     }
 }
