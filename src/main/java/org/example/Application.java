2dbef0ff391fd5e1d0dad78582b53fec9ac20637
Art <you@example.com>
2020-11-30 05:51:08 +0200

    Add Application.java

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import quickfix.*;
+
+public interface Application
+{
+    void onCreate( SessionID sessionID );
+
+    void onLogon( SessionID sessionID );
+
+    void onLogout( SessionID sessionID );
+
+    void toAdmin( Message message, SessionID sessionID );
+    void toApp( Message message, SessionID sessionID ) throws DoNotSend;
+    void fromAdmin( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon;
+    void fromApp( Message message, SessionID sessionID ) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon;
+
+
+}
