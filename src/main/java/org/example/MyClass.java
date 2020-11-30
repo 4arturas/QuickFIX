663288933d5d88b78e18888744da81b8c2562127
Art <you@example.com>
2020-11-30 11:13:31 +0200

    Update MyClass.java

--- src/main/java/org/example/MyClass.java
+++ src/main/java/org/example/MyClass.java
@@ -1,5 +1,36 @@
 package org.example;
 
+import quickfix.*;
+
+import java.io.FileInputStream;
+
 public class MyClass
 {
+    public static void main( String[] args ) throws Exception
+    {
+        if ( args.length != 1 )
+            return;
+
+        String fileName = args[0];
+
+        quickfix.Application application = new FooApplication();
+        SessionSettings sessionSettings = new SessionSettings( new FileInputStream( fileName ) );
+        MessageStoreFactory messageStoreFactory = new FileStoreFactory( sessionSettings );
+        LogFactory logFactory = new FileLogFactory( sessionSettings );
+        MessageFactory messageFactory = new DefaultMessageFactory();
+        Acceptor acceptor = new SocketAcceptor( application, messageStoreFactory, sessionSettings, logFactory, messageFactory );
+        acceptor.start();
+        try
+        {
+//        while ( 1 == 1 )
+            {
+                System.out.println( "1" );
+            }
+        }
+        finally
+        {
+            acceptor.stop();
+        }
+
+    }
 }
